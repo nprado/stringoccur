f21a457a1ec88c552d50340dd18ea4ab7acc01ef
Daniel Prado Borges Silva <ra032018@scooby.lab.ic.unicamp.br>
2011-03-17 17:09:53 -0300

    first commit

---
+++ stringoccur.py
@@ -0,0 +1,11 @@
+a = " Dramatic advances in data capture, processing power, data transmission, and storage capabilities are enabling organizations to integrate their various databases into data warehouses. Data warehousing is defined as a process of centralized data management and retrieval. Data warehousing, like data mining, is a relatively new term although the concept itself has been around for years. Data warehousing represents an ideal vision of maintaining a central repository of all organizational data. Centralization of data is needed to maximize user access and analysis. Dramatic technological advances are making this vision a reality for many companies. And, equally dramatic advances in data analysis software are allowing users to access this data freely. The data analysis software is what supports data mining "
+
+print a.count("data")
+a = a.split()
+for i in a: 
+	print  i,":",  a.count(i), "ocorrencias", "\n" 
+
+
+
+
+
